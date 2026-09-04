2f64789d249c07f18e5c99960f20760737aeefc0
juanesgl <juan.sgarcia@mail.escuelaing.edu.co>
2026-09-04 11:14:08 -0500

    chore: nuevo nombre de la clase

--- src/main/java/edu/eci/arsw/math/PiDigitsThreads.java
+++ src/main/java/edu/eci/arsw/math/PiDigitsThreads.java
@@ -1,18 +1,18 @@
 package edu.eci.arsw.math;
 
-public class PrimeThreads extends Thread {
+public class PiDigitsThreads extends Thread {
     private int start;
     private int numPlusOne;
     private byte[] num;
-    private PrimeThreads prime;
+    private PiDigitsThreads prime;
 
-    public PrimeThreads(int start, int numPlusOne) {
+    public PiDigitsThreads(int start, int numPlusOne) {
         this.start = start;
         this.numPlusOne = numPlusOne;
     }
 
     public void run(){
-        this.prime = new PrimeThreads(start, numPlusOne);
+        this.prime = new PiDigitsThreads(start, numPlusOne);
     }
 
     public void getNum(byte[] num){
